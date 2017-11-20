91ea6e0f1bca27ea6d60e283d7e176c6818135fd
shavit <shavit@users.noreply.github.com>
2017-11-20 01:48:01 -0500

    🙈

--- rapidvideo.go
+++ rapidvideo.go
@@ -16,22 +16,50 @@ type response struct {
   Result interface{} `json: "result"`
 }
 
+type videoMeta struct {
+  Code string `json: "code"`
+  Name string `json: "name"`
+  Description string `json: "description"`
+}
+
+type uploadStatus struct {
+  TotalFilesize string `json: "total_filesize"`
+  TransferFilesize string `json: "transfer_filesize"`
+  Progress string `json: "progress"`
+  Done string `json: "done"`
+  ObjectCode string `json: "object_code"`
+}
+
 type Rapidvideo interface {
   // get make a GET request to the api
   get(endpoint string) (resp *response, err error)
 
   // SetProxy set a proxy URL
   SetProxy(u string) (err error)
+
+  // GetInfo check if a video is online
+  GetInfo(code string) (v *videoMeta, err error)
+
+  // Upload upload a video file and receive the video id
+  Upload(path string) (err error)
+
+  // RemoteUpload remote upload a file
+  RemoteUpload(url string) (ok bool, err error)
+
+  // RemoteStatus check the status of the remote upload
+  RemoteStatus(id string) (status *uploadStatus, err error)
 }
 
 type rapidvideo struct {
+  userId string
   apiKey string
   client *http.Client
 }
 
 // Craete a new rapidvideo
-func NewRapidVideo(apiKey string) Rapidvideo {
+func NewRapidVideo(userId, apiKey string) Rapidvideo {
   return &rapidvideo{
+    userId: userId,
     apiKey: apiKey,
     client: new(http.Client),
   }
@@ -77,4 +105,44 @@ func (rv *rapidvideo) SetProxy(u string) (err error) {
   rv.client.Transport = &http.Transport{Dial: dialer.Dial}
 
   return err
+}
+
+// GetInfo check if a video is online
+func (rv *rapidvideo) GetInfo(code string) (v *videoMeta, err error) {
+  resp, err := rv.get(fmt.Sprintf("//objects.php?ac=info&apikey=%v&code=%v", rv.apiKey, code))
+  if err != nil {
+    return v, err
+  }
+
+  if resp.Status != 200 {
+    return v, errors.New(resp.Msg)
+  }
+
+  body, err := json.Marshal(resp.Result)
+  if err != nil {
+    return v, err
+  }
+
+  if err = json.Unmarshal(body, &v); err != nil {
+    return v, err
+  }
+
+  return v, err
+}
+
+// Upload upload a video file and receive the video id
+func (rv *rapidvideo) Upload(path string) (err error) {
+  return err
+}
+
+// RemoteUpload remote upload a file
+func (rv *rapidvideo) RemoteUpload(url string) (ok bool, err error) {
+  return ok, err
+}
+
+// RemoteStatus check the status of the remote upload
+func (rv *rapidvideo) RemoteStatus(id string) (status *uploadStatus, err error){
+  status = new(uploadStatus)
+  
+  return status, err
 }
--- rapidvideo_test.go
+++ rapidvideo_test.go
@@ -6,14 +6,20 @@ import (
 )
 
 func NewTestRapidVideo() (rv Rapidvideo){
+  var userId = os.Getenv("RAPID_VIDEO_USER_ID")
   var apiKey = os.Getenv("RAPID_VIDEO_API_KEY")
 
-  return NewRapidVideo(apiKey)
+  return NewRapidVideo(userId, apiKey)
 }
 
 func TestCreateRapidVideo(t *testing.T){
+  var userId = os.Getenv("RAPID_VIDEO_USER_ID")
   var apiKey = os.Getenv("RAPID_VIDEO_API_KEY")
   var rv Rapidvideo
+
+  if userId == "" {
+    t.Error("RAPID_VIDEO_USER_ID is not defined")
+  }
 
   if apiKey == "" {
     t.Error("RAPID_VIDEO_API_KEY is not defined")
@@ -37,5 +43,53 @@ func TestSetProxy(t *testing.T){
   err = rv.SetProxy("socks5://127.0.0.1:9050")
   if err != nil {
     t.Error("Error setting a proxy:", err)
+  }
+}
+
+func TestGetInfo(t *testing.T) {
+  var rv Rapidvideo = NewTestRapidVideo()
+
+  v, err := rv.GetInfo("code")
+  if err != nil {
+    t.Error("Error upload a file", err.Error())
+  }
+
+  if v.Code == "" {
+    t.Error("Empty video code")
+  }
+}
+
+func TestUpload(t *testing.T) {
+  var rv Rapidvideo = NewTestRapidVideo()
+
+  err := rv.Upload("")
+  if err != nil {
+    t.Error("Error upload a file", err.Error())
+  }
+}
+
+func TestRemoteUpload(t *testing.T) {
+  var rv Rapidvideo = NewTestRapidVideo()
+
+  ok, err := rv.RemoteUpload("url")
+  if err != nil {
+    t.Error("Error creating a remote file upload", err.Error())
+  }
+
+  if ok != true {
+    t.Error("Should create a remote upload")
+  }
+}
+
+func TestRemoteStatus(t *testing.T) {
+  var rv Rapidvideo = NewTestRapidVideo()
+
+  status, err := rv.RemoteStatus("id")
+  if err != nil {
+    t.Error("Error getting a remote file upload status", err.Error())
+  }
+
+  if status.ObjectCode == "" {
+    t.Error("Should get an object code")
   }
 }
